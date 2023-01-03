3c9aa07a6a92ea7cdaa699a95eebf3180919cde1
Chanmoey <relijifa@gmail.com>
2023-01-04 03:28:39 +0800

    创建项目，关联github远程仓库。

---
+++ src/main/java/com/moon/storagering/StorageRingApplication.java
@@ -0,0 +1,13 @@
+package com.moon.storagering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageRingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageRingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moon/storagering/StorageRingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.storagering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageRingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
